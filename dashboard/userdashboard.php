0f3947df7692388bef058d303c3ee795080bdcdb
aetheryle <ryleaether@gmail.com>
2026-04-23 08:27:39 +0800

    Restore dashboard only

---
+++ dashboard/userdashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../design/mainstyle.css">
+</head>
+<body>
+
+<h1>Welcome to Menu</h1>
+
+<!-- display menu items here -->
+
+</body>
+</html>
